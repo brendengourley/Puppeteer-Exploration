8c3814624e6b61aadc73d59702641cba242952f2
Brenden Gourley <brendengourley@gmail.com>
2020-08-03 14:14:39 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const puppeteer = require('puppeteer');
+
+( async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('https://www.amazon.com/Best-Sellers-Electronics-Televisions/zgbs/electronics/172659')
+  const names = await page.$$eval('.p13n-sc-truncated', items => {
+    return items.map( item => item.textContent )
+  })
+  const prices = await page.$$eval('.p13n-sc-price', items => {
+    return items.map( item => item.textContent )
+  })
+  //console.log(names)
+  //console.log(prices)
+  Object.entries(names).forEach((key, value) => {
+    console.log(`Product: ${key[1]}\n       Price: ${prices[value]}`)
+  })
+  await browser.close()
+} )();
